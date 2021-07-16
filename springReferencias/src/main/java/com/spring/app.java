7d14eef160123c11727519e8f8bb970d14fdc99b
GAbriel Pérez Rodríguez <gabrielpr93@hotmail.com>
2021-07-16 17:59:26 +0200

    Beans que referencian a otros beans

--- springReferencias/src/main/java/com/spring/app.java
+++ springReferencias/src/main/java/com/spring/app.java
@@ -6,21 +6,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class app {
 
 	public static void main(String[] args) {
-		
-	ApplicationContext appContext = new ClassPathXmlApplicationContext("beans.xml");
-		
-		Saludator saludo= null;
 
-		saludo=appContext.getBean(Saludator.class);
-		
-		System.out.println(saludo.saludo()+"\n");
-		
-		EmailService emailService=null;
-		
-		emailService=appContext.getBean(EmailService.class);
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("beans.xml");
+
+		Saludator saludo = null;
+
+		saludo = appContext.getBean(Saludator.class);
+
+		System.out.println(saludo.saludo() + "\n");
+
+		EmailService emailService = null;
+
+		emailService = appContext.getBean(EmailService.class);
 		emailService.enviarEmailSaludos("gabrielpr93@hotmail.com");
-		
-		((ClassPathXmlApplicationContext)appContext).close();
+
+		((ClassPathXmlApplicationContext) appContext).close();
 	}
 
 }
