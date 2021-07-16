2587518c94d95290932a37af44eaaac2b36e2b23
GAbriel Pérez Rodríguez <gabrielpr93@hotmail.com>
2021-07-16 17:25:36 +0200

    InyeccionSetter

---
+++ springSetter/src/main/java/com/spring/Saludator.java
@@ -0,0 +1,16 @@
+package com.spring;
+
+public class Saludator {
+	
+	private String mensaje;
+	
+	public void setMensaje(String str) {
+		this.mensaje=str.toUpperCase();
+	}
+	
+	
+	public String saludo() {
+		return (mensaje==null) ? "Hola Mundo":mensaje;
+	}
+
+}
