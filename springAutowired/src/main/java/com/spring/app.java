9b4e1ca18cf90c0c356c9832b84253d150cac4eb
GAbriel Pérez Rodríguez <gabrielpr93@hotmail.com>
2021-07-19 13:18:01 +0200

    Inyeccion automática

---
+++ springAutowired/src/main/java/com/spring/app.java
@@ -0,0 +1,28 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class app {
+
+	public static void main(String[] args) {
+		
+	ApplicationContext appContext = new ClassPathXmlApplicationContext("beans.xml");
+//
+//	Saludator saludo = null;
+//
+//	saludo = appContext.getBean(Saludator.class);
+//
+//	System.out.println(saludo.saludo() + "\n");
+
+	IEmailService emailService = null;
+
+	emailService = appContext.getBean(IEmailService.class);
+	emailService.enviarSaludo("gabrielpr93@hotmail.com");
+	
+
+	((ClassPathXmlApplicationContext) appContext).close();
+
+	}
+
+}
