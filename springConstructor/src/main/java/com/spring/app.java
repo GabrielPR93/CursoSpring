cb375cdd9494149a68d27e4deb254d960ced2fc9
GAbriel Pérez Rodríguez <gabrielpr93@hotmail.com>
2021-07-21 18:00:55 +0200

    InyeccionConstructor

--- springConstructor/src/main/java/com/spring/app.java
+++ springConstructor/src/main/java/com/spring/app.java
@@ -6,6 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class app {
 
 	public static void main(String[] args) {
+		
 		ApplicationContext appContext = new ClassPathXmlApplicationContext("beans.xml");
 
 		Saludator saludo = null;
