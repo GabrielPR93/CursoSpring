70a789549a997612f8c2f411f4a7ad152764016e
GAbriel Pérez Rodríguez <gabrielpr93@hotmail.com>
2021-07-24 13:39:43 +0200

    Uso de @PostConstruct y @PreDestroy

---
+++ springPostConstructPreDestroy/src/main/java/com/spring/App.java
@@ -0,0 +1,22 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("beans.xml");
+
+		PeliculaService peliculaService = appContext.getBean(PeliculaService.class);
+
+		for (Pelicula peli : peliculaService.pelisPorGenero("Ciencia ficción")) {
+			System.out.println(peli.getTitulo());
+		}
+
+		((ClassPathXmlApplicationContext) appContext).close();
+
+	}
+
+}
