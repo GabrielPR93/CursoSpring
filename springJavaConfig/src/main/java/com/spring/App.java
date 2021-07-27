e83c814de1c661f21e754ea5bd2a2bf296d4f177
GAbriel Pérez Rodríguez <gabrielpr93@hotmail.com>
2021-07-27 13:04:10 +0200

    Uso de @Configuration

---
+++ springJavaConfig/src/main/java/com/spring/App.java
@@ -0,0 +1,19 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		
+	ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		Saludator saludator=appContext.getBean(Saludator.class);
+		
+		System.out.println(saludator.saludo());
+		
+		((AnnotationConfigApplicationContext)appContext).close();
+	}
+}
