1202c642f33f439f7570258cab5546a670234a08
GAbriel Pérez Rodríguez <gabrielpr93@hotmail.com>
2021-07-29 13:13:31 +0200

    Implementaciones

---
+++ MovieAdvisor/src/main/java/com/movieAdvisor/config/AppConfig.java
@@ -0,0 +1,36 @@
+package com.movieAdvisor.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan(basePackages = "com.movieAdvisor")
+@PropertySource("classpath:/movieAdvisor.properties")
+public class AppConfig {
+	
+	@Value("${file.path}")
+	private String file;
+	
+	@Value("${file.csv.separator}")
+	private String separator;
+
+	@Value("${file.csv.list_separator}")
+	private String listSeparator;
+
+	public String getFile() {
+		return file;
+	}
+
+	public String getSeparator() {
+		return separator;
+	}
+
+	public String getListSeparator() {
+		return listSeparator;
+	}
+	
+	
+	
+}
