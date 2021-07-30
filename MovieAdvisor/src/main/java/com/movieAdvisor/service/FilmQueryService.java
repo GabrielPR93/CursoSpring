247694bf0c326bdc990e578bc703af5a208404f1
GAbriel Pérez Rodríguez <gabrielpr93@hotmail.com>
2021-07-30 13:40:34 +0200

    Implementaciones MovieAdvisor

--- MovieAdvisor/src/main/java/com/movieAdvisor/service/FilmQueryService.java
+++ MovieAdvisor/src/main/java/com/movieAdvisor/service/FilmQueryService.java
@@ -1,5 +1,23 @@
 package com.movieAdvisor.service;
 
+import java.util.Collection;
+
+import com.movieAdvisor.model.Film;
+
 public interface FilmQueryService {
 
+	
+	public Collection<Film> exec();
+	
+	public FilmQueryService anyGenre(String... genres);
+	
+	public FilmQueryService allGenres(String... genres);
+	
+	public FilmQueryService year(String year);
+	
+	public FilmQueryService betweenYears(String from,String to);
+	
+	public FilmQueryService titleContains(String title);
+
+	
 }
